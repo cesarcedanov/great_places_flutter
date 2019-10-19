a049118f399d3e488ad1e4211b8bf4a6ac251bc8
CesarCedanoV <CesarCedanoV@gmail.com>
2019-10-19 10:46:11 -0400

    Created a screen where we can open the camera or the photo Gallery to preview an Image

---
+++ great_places/lib/providers/great_places.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/foundation.dart';
+import '../models/place.dart';
+
+class GreatPlaces with ChangeNotifier {
+  List<Place> _places = [];
+
+  List<Place> get places {
+    return [..._places];
+  }
+}
---
+++ great_places/lib/widgets/image_input.dart
@@ -0,0 +1,96 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as path_provider;
+
+class ImageInput extends StatefulWidget {
+  final Function onSelectImage;
+
+  ImageInput(this.onSelectImage);
+
+  @override
+  _ImageInputState createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  File _storedImage;
+
+  Future<void> _takePicture() async {
+    final imageFile = await ImagePicker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+    setState(() {
+      _storedImage = imageFile;
+    });
+    final appDir = await path_provider.getApplicationDocumentsDirectory();
+    final fileName = path.basename(imageFile.path);
+    final savedImage = await imageFile.copy('${appDir.path}/${fileName}');
+    widget.onSelectImage(savedImage);
+  }
+
+  Future<void> _openPhotoLibrary() async {
+    final imageFile = await ImagePicker.pickImage(
+      source: ImageSource.gallery,
+      maxWidth: 600,
+    );
+    setState(() {
+      _storedImage = imageFile;
+    });
+    final appDir = await path_provider.getApplicationDocumentsDirectory();
+    final fileName = path.basename(imageFile.path);
+    final savedImage = await imageFile.copy('${appDir.path}/${fileName}');
+    widget.onSelectImage(savedImage);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          width: 200,
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              color: Colors.grey,
+            ),
+          ),
+          alignment: Alignment.center,
+          child: _storedImage != null
+              ? Image.file(
+                  _storedImage,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                )
+              : Text(
+                  'No Image',
+                  textAlign: TextAlign.center,
+                ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Expanded(
+          child: Column(
+            children: <Widget>[
+              FlatButton.icon(
+                icon: Icon(Icons.photo_camera),
+                label: Text('Take a Picture'),
+                textColor: Theme.of(context).primaryColor,
+                onPressed: _takePicture,
+              ),
+              FlatButton.icon(
+                icon: Icon(Icons.photo_library),
+                label: Text('Opeh Photo Library'),
+                textColor: Theme.of(context).primaryColor,
+                onPressed: _openPhotoLibrary,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
